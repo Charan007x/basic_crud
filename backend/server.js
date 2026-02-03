eb41502e68c29f1805a2e661b7c77211d0220127
Charan007x <saicharangottipalli@gmail.com>
2026-02-03 22:56:37 +0530

    Forgot to commit server.js, part of Database configuration

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import notesRoutes from './src/routes/notesRoutes.js';
+import {connectDB} from './src/config/db.js';
+
+connectDB();
 const app=express();
 
 app.use('/api/notes', notesRoutes);
